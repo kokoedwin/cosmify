683a2165c04d32dcdd6dcd2638624ae300146851
Jonathan Edwin <edwin.ryth@gmail.com>
2023-03-14 00:21:08 +0700

    Modified the PSP

--- script_psp.js
+++ script_psp.js
@@ -1,9 +1,10 @@
-var products = [
-  {name: "Product 1", skinType: "normal", ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"]},
+var products = [  {name: "Product 1", skinType: "normal", ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"]},
   {name: "Product 2", skinType: "oily", ingredients: ["Ingredient 2", "Ingredient 4", "Ingredient 5"]},
   {name: "Product 3", skinType: "dry", ingredients: ["Ingredient 1", "Ingredient 3", "Ingredient 6"]},
   {name: "Product 4", skinType: "combination", ingredients: ["Ingredient 4", "Ingredient 5", "Ingredient 6"]},
 ];
+
+var productCheckboxes = [];
 
 function displayProducts(products) {
   var productList = document.getElementById("productList");
@@ -15,6 +16,9 @@ function displayProducts(products) {
     checkbox.type = "checkbox";
     checkbox.name = "productCheckbox";
     checkbox.value = product.name;
+    if (productCheckboxes.includes(product.name)) {
+      checkbox.checked = true;
+    }
     checkbox.addEventListener("click", enableAddButton);
     var label = document.createElement("label");
     label.innerHTML = product.name + " (" + product.skinType + ")";
@@ -44,7 +48,27 @@ function disableAddButton() {
   addButton.disabled = true;
 }
 
+function saveCheckboxState() {
+  var productList = document.getElementById("productList");
+  var checkboxes = productList.getElementsByTagName("input");
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      var productName = checkboxes[i].value;
+      if (!productCheckboxes.includes(productName)) {
+        productCheckboxes.push(productName);
+      }
+    } else {
+      var productName = checkboxes[i].value;
+      var index = productCheckboxes.indexOf(productName);
+      if (index > -1) {
+        productCheckboxes.splice(index, 1);
+      }
+    }
+  }
+}
+
 function goToConflictCheckPage() {
+  saveCheckboxState();
   var selectedProducts = [];
   var productList = document.getElementById("productList");
   var checkboxes = productList.getElementsByTagName("input");
