efdc1afec251e10e65f67d32ab2cc9ee33a60eac
Jonathan Edwin <edwin.ryth@gmail.com>
2023-03-14 17:19:46 +0700

    Add feature to show the product name that is conflict

--- script_cp.js
+++ script_cp.js
@@ -12,20 +12,23 @@ function displaySelectedProducts() {
 
 function checkForConflicts() {
   var selectedProducts = JSON.parse(sessionStorage.getItem("selectedProducts"));
-  var ingredientSet = new Set();
+  var ingredientMap = new Map();
   for (var i = 0; i < selectedProducts.length; i++) {
     var product = selectedProducts[i];
     for (var j = 0; j < product.ingredients.length; j++) {
       var ingredient = product.ingredients[j];
-      if (ingredientSet.has(ingredient)) {
-        document.getElementById("conflictResult").innerHTML = "There is a conflict between the selected products.";
+      if (ingredientMap.has(ingredient)) {
+        var conflictingProduct = ingredientMap.get(ingredient);
+        var conflictMsg = "There is a conflict between " + product.name + " and " + conflictingProduct.name + " due to ingredient: " + ingredient + ".";
+        document.getElementById("conflictResult").innerHTML = conflictMsg;
         return;
       }
-      ingredientSet.add(ingredient);
+      ingredientMap.set(ingredient, product);
     }
   }
   document.getElementById("conflictResult").innerHTML = "There are no conflicts between the selected products.";
 }
+
 
 function goToSelectionPage() {
   sessionStorage.removeItem("selectedProducts");
