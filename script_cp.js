2fc99ec4955360a1e163fb932a38d9f0753c9e5e
Jonathan Edwin <edwin.ryth@gmail.com>
2023-03-14 17:34:40 +0700

    Add conflict Map

--- script_cp.js
+++ script_cp.js
@@ -1,3 +1,11 @@
+var conflictMap = {
+  "Ingredient1": new Set(["Ingredient2", "Ingredient3"]),
+  "Ingredient2": new Set(["Ingredient1", "Ingredient4"]),
+  "Ingredient3": new Set(["Ingredient1"]),
+  "Ingredient4": new Set(["Ingredient2"])
+};
+
+
 function displaySelectedProducts() {
   var productList = document.getElementById("productList");
   productList.innerHTML = "";
@@ -9,22 +17,38 @@ function displaySelectedProducts() {
     productList.appendChild(li);
   }
 }
-
 function checkForConflicts() {
   var selectedProducts = JSON.parse(sessionStorage.getItem("selectedProducts"));
   var ingredientMap = new Map();
+  var conflictingIngredientsMap = new Map();
   for (var i = 0; i < selectedProducts.length; i++) {
     var product = selectedProducts[i];
     for (var j = 0; j < product.ingredients.length; j++) {
       var ingredient = product.ingredients[j];
       if (ingredientMap.has(ingredient)) {
         var conflictingProduct = ingredientMap.get(ingredient);
-        var conflictMsg = "There is a conflict between " + product.name + " and " + conflictingProduct.name + " due to ingredient: " + ingredient + ".";
+        var conflictingIngredients = conflictingIngredientsMap.get(conflictingProduct) || new Set();
+        conflictingIngredients.add(ingredient);
+        conflictingIngredientsMap.set(conflictingProduct, conflictingIngredients);
+        var conflictMsg = "There is a conflict between " + product.name + " and " + conflictingProduct.name + " due to ingredients: " + Array.from(conflictingIngredients).join(", ") + ".";
         document.getElementById("conflictResult").innerHTML = conflictMsg;
         return;
       }
       ingredientMap.set(ingredient, product);
+      var conflictingIngredients = conflictMap[ingredient] || new Set();
+      for (var conflictingIngredient of conflictingIngredients) {
+        if (ingredientMap.has(conflictingIngredient)) {
+          var conflictingProduct = ingredientMap.get(conflictingIngredient);
+          var conflictingIngredients = conflictingIngredientsMap.get(conflictingProduct) || new Set();
+          conflictingIngredients.add(ingredient);
+          conflictingIngredientsMap.set(conflictingProduct, conflictingIngredients);
+          var conflictMsg = "There is a conflict between " + product.name + " and " + conflictingProduct.name + " due to ingredients: " + Array.from(conflictingIngredients).join(", ") + ".";
+          document.getElementById("conflictResult").innerHTML = conflictMsg;
+          return;
+        }
+      }
     }
+    conflictingIngredientsMap.set(product, new Set());
   }
   document.getElementById("conflictResult").innerHTML = "There are no conflicts between the selected products.";
 }
